7f36847b6f7fe97ce31d8fcc491afa2828b8008d
DagioGit <oidagioc@gmail.com>
2025-11-25 19:38:36 -0600

    dashboard medium

--- server.js
+++ server.js
@@ -216,7 +216,7 @@ app.get("/auth/callback", async (req, res) => {
             { expiresIn: "7d" }
         );
 
-        res.redirect(`http://localhost:5173/login?token=${jwtToken}`);
+        res.redirect(`spritlebot.netlify.app/login?token=${jwtToken}`);
     } catch (err) {
         console.error("OAuth error:", err?.response?.data || err);
         res.status(500).send("Error en OAuth");
